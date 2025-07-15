65666ea94197246af3ea7b887bda6f1028462978
Sippiairborne <claimsmasters0@gmail.com>
2025-07-15 14:10:49 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🌪️ Welcome to StormForce</h1>
+      <p>The ultimate storm response and recovery app.</p>
+    </div>
+  );
+}
+
+export default App;
